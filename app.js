8c1075bb3d0ab6f25deb96f347cb49dbe6efe87c
baburali <baburali1910@gmail.com>
2024-07-22 21:01:25 -0700

    add changes

---
+++ app.js
@@ -0,0 +1,92 @@
+// console.log("hello function");
+
+// task-1 === write a function to check if the number is even or odd
+// const userinput = +prompt("enter the number")
+
+// function checkNumbers(userinput) {
+//     if (userinput %2 === 0) {
+//         console.log("even");
+//     }else{
+//         console.log("odd");
+//     }
+//     }
+
+//     checkNumbers(userinput)
+
+// <=================================>
+
+// task-2 === calculate the square and return the result
+// const userinput = +prompt("enter the number")
+
+// function calculateSquare(number) {
+//     return number * number
+// }
+
+// const result = calculateSquare(userinput)
+// console.log("the square of the number = " , result);
+
+// <=================================>
+
+// task-3 === find the max of two number
+// task-4 === concatenate two string and return the result
+
+// const str1 = prompt("enter the 1st str")
+// const str2 = prompt("enter the 2nd str")
+
+// function addTwoStr(str1 , str2) {
+//     return str1 + str2
+// }
+// const result = addTwoStr(str1 , str2)
+// console.log("concatenation of two string ", result);
+
+// <==================================>
+
+// task-5 === write an arrow function and calculte the sum of two numbers
+// const num1 = +prompt("enter the 1st  number")
+// const num2 = +prompt("enter the 2nd number")
+// const calculateSum = (num1, num2)=>{
+// return num1 + num2
+// }
+
+// const sum = calculateSum(num1, num2)
+// console.log("the sum of two numbers =" , sum);
+
+// <==================================>
+
+// task-6 === write an arrow function to is the string contain any special character
+
+// const str = prompt("enter the string")
+// const containSpecialCharacter = (str)=> /[!@#\$%\^\&*\)\(+=._-]/.test(str)
+
+
+// console.log(containSpecialCharacter(str));
+
+// <==================================>
+
+// task-7 === write an arrow function and calculte the sum of two numbers
+
+// const firstNum = +prompt("enter the 1st number")
+// const secondNum = +prompt("enter the 2nd number")
+// const multiplyOfTwo = (a , b = 2)=> a * b;
+// console.log(multiplyOfTwo(firstNum , secondNum));
+
+// <==================================>
+
+// task-8 === write a function that takes person name and age and give a greeting message and give default age
+
+// const userName = prompt("enter the your name")
+// const userAge = +prompt("enter the your age")
+
+// function greetUser(userName, userAge = 24) {
+//     console.log(`hello dear ${userName} is this your age ${userAge}`);
+// }
+// greetUser(userName, userAge)
+
+
+// <==================================>
+
+
+
+
+
+
